b099ee25bef93c6e8f442812fc060442286a0917
Zegis <kjgm@poczta.onet.pl>
2018-06-20 22:22:58 +0200

    Simple server that returns html and css static files

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const http = require('http');
+const fs = require('fs');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => 
 {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end("Goodbye galaxy!");
+
+    if(req.url.includes('.css'))
+    {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/css');
+        fs.createReadStream("content\\main.css").pipe(res);
+    }
+    else if(req.url == '/' || req.url.includes('.html'))
+    {
+
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        fs.createReadStream("view\\index.html").pipe(res);
+    }
+    return;
 });
 
 server.listen(port, hostname, () => {
